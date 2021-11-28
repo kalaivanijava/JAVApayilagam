b2261da9142cc800d82db78f4cd35cc4b423b021
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-28 17:38:37 +0800

    Add files via upload

---
+++ India.java
@@ -0,0 +1,20 @@
+package tamilnadu.chennai;
+
+public abstract class India {
+	static String capital="newdelhi";
+	public abstract void speaklanguage();
+	public abstract void eat();
+	public abstract void dress();
+	public India(String primeminister)
+{
+		
+		System.out.println("our prime minister is "+primeminister);
+	}
+	public India()
+	{
+		
+	}
+	
+	
+
+}
