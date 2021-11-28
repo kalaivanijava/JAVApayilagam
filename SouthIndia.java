cf348440f1af0093eeb54eebbfb3ece315116fcc
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-28 17:38:54 +0800

    Add files via upload

---
+++ SouthIndia.java
@@ -0,0 +1,25 @@
+package tamilnadu.chennai;
+
+public abstract class SouthIndia extends India {
+	
+			
+	
+	public SouthIndia(String primeminister) {
+		super(primeminister);
+		// TODO Auto-generated constructor stub
+	}
+	public SouthIndia()
+	{
+		
+	}
+	public void cultivate()
+	{
+		System.out.println("Rice cultivation //southindia");
+	}
+	public void livingstyle()
+	{
+		System.out.println("Average Development //southindia");
+	}
+	
+
+}
