837a20a8fb48b50b123b3dd155bef10fbcdeee72
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-28 17:37:53 +0800

    Add files via upload

---
+++ Actor.java
@@ -0,0 +1,11 @@
+package tamilnadu.chennai;
+
+public interface Actor {
+	boolean makeuprequired=true;
+	String address="chennai";
+	
+	public void act();
+	public void sing();
+	public void dance();
+
+}
